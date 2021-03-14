efd1cf4f55ced9cde0092ef205b3d89469c44d7b
Zachary Telschow <zacharytelschow@gmail.com>
2021-03-14 13:50:56 -0400

    Adding notes for using my repo.

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -3,6 +3,8 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
     env: {
         contentApi: 'labs.cdn.content.amplience.net'
+        //default: labs.cdn.content.amplience.net
+        //mine: machathon2.cdn.content.amplience.net
     },
     poweredByHeader: false
 }
